c4b455a6738ee0ee8551d5b614c5a67671a3ee84
Luca <lucablazevic@yahoo.de>
2022-02-14 06:14:35 +0100

    add node relation parent

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,44 +1,61 @@
+MOVES = [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2]]
+
 class Node
-  attr_accessor :children, :field
+  attr_accessor :children, :field, :parent
 
   def initialize(field)
     @field = field
     @children = []
+    @parent = nil
   end
 end
 
 class Tree
-  def initialize(root)
+  attr_accessor :root
+
+  def initialize(root, board)
     @root = Node.new(root)
+    @board = board
   end
 
-  def build_tree(arr, knight, root = @root)
-    knight.moves.each do |move|
-      p move[0] + root.field[0]
-      p move[1] + root.field[1]
-      puts
+  def build_tree(root = @root)
+    return if @board.missing_fields.empty?
+
+    find_children(root)
+    root.children.each do |child|
+      find_children(child)
+    end
+    root.children.each do |child|
+      build_tree(child)
+    end
+  end
+
+  def find_children(root = @root)
+    MOVES.each do |move|
       if (root.field[0] + move[0] >= 0 || root.field[0] + move[0] <= 7) && (root.field[1] + move[1] >= 0 || root.field[1] + move[1] <= 7)
         new_child = [root.field[0] + move[0], root.field[1] + move[1]]
-        p "new child is #{new_child} #{arr.include?(new_child)}"
-        if arr.include?(new_child)
-          p 'hallo'
-          root.children.push(Node.new(new_child))
-          arr.delete(new_child)
+        if @board.missing_fields.include?(new_child)
+          @board.missing_fields.delete(new_child)
+          new_child = Node.new(new_child)
+          new_child.parent = root
+          root.children.push(new_child)
         end
       end
     end
-    p arr
   end
 
-  def print_tree(root = @root, spaces = '')
-    return if root.nil?
+  def preorder(target, way = [], root = @root)
+    return root if root.nil?
 
-    puts "#{spaces}#{root.field}"
-    return if root.children.empty?
-
-    puts "#{spaces}children"
-    spaces += '  '
-    root.children.each { |child| print_tree(child, spaces) }
+    if root.field == target
+      way.push(root.field) unless way.include?(target)
+      until way.include?(@root.field)
+        way.push(root.parent.field)
+        root = root.parent
+      end
+    end
+    root.children.each { |child| preorder(target, way, child) }
+    way
   end
 end
 
@@ -64,24 +81,28 @@ class Board
   def create_missing_fields
     arr = []
     @fields.each do |x|
-      x.each {|y| arr.push(y)}
+      x.each { |y| arr.push(y) }
     end
     arr
   end
 end
 
 class Knight
-  attr_accessor :moves
-
-  def initialize(position)
-    @moves = [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2]]
+  def initialize(position = [0, 0])
     @position = position
-    @possible_moves = []
+    @board = Board.new
+    @tree = Tree.new(position, @board)
+  end
+
+  def knight_moves(start, destination)
+    @position = start
+    @tree.root = Node.new(start)
+    @tree.build_tree
+    shortest_path = @tree.preorder(destination).reverse
+    puts "=> You made it in #{shortest_path.length - 1} moves! Here's your path:"
+    shortest_path.each { |field| p field }
   end
 end
 
-b = Board.new
-t = Tree.new([3, 3])
-t.build_tree(b.missing_fields, Knight.new([3, 3]))
-p b.missing_fields
-p t.print_tree
+k = Knight.new
+k.knight_moves([3, 3], [7, 7])
